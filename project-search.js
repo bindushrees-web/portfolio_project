68d26951fe39a93bc7952d2a52901eff4951d667
bindushrees-web <bindushrees754@gmail.com>
2026-05-05 09:54:24 +0530

    Deleted Files

--- project-search.js
+++
@@ -1,9 +0,0 @@
-function initProjectSearch() {
-    const searchInput = document.getElementById("project-search");
-
-    if (!searchInput) return;
-
-    searchInput.addEventListener("input", function () {
-        applyFilters();
-    });
-}
